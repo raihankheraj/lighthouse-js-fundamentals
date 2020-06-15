45e7d2a9a64055cb82e21124260a2af4d0c488aa
raihankheraj <rkheraj@hotmail.com>
2020-06-15 13:30:55 -0700

    Hello world function

---
+++ hello.js
@@ -0,0 +1,8 @@
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+const returnSayHello = function(name){
+ return "Hello, " + name;
+}
+const greeting = returnSayHello("Raihan);
+console.log(greeting);
